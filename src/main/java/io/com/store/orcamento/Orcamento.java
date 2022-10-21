141d0b41a50ed39090ea9f256d3ffd70466328c6
flavio.santana <9090flavio@gmail.com>
2022-10-21 16:53:17 -0300

    feat: adicionando a situação do orçamento

--- src/main/java/io/com/store/orcamento/Orcamento.java
+++ src/main/java/io/com/store/orcamento/Orcamento.java
@@ -4,12 +4,27 @@ import java.math.BigDecimal;
 
 public class Orcamento {
 
-    private final BigDecimal valor;
+    private BigDecimal valor;
     private final int quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+        if (situacao.equals("EM_ANALISE")) {
+            valorDoDescontoExtra = valor.multiply(new BigDecimal("0.05"));
+        } else if (situacao.equals("APROVADO")) {
+            valorDoDescontoExtra = valor.multiply(new BigDecimal("0.02"));
+        }
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovado() {
+        this.situacao = "APROVADO";
     }
 
     public BigDecimal getValor() {
