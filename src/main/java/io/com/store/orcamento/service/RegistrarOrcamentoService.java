8285dab368f1cf0087c98fe55c9640fd4c081eab
flavio.santana <9090flavio@gmail.com>
2022-10-24 12:10:21 -0300

    feat: api de registro de orçamento

---
+++ src/main/java/io/com/store/orcamento/service/RegistrarOrcamentoService.java
@@ -0,0 +1,11 @@
+package io.com.store.orcamento.service;
+
+import io.com.store.orcamento.Orcamento;
+
+public class RegistrarOrcamentoService {
+
+    public void registrar(Orcamento orcamento) {
+        //IMPLMENTAÇLÃO FUTURA DE CHAMADA DE API EXTERNA
+    }
+
+}
